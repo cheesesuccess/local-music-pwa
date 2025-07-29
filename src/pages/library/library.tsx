f4e9438ba2ff9b647c3010047da75c4898c1e563
cheesesuccess <natashasibanda2025@outlook.com>
2025-07-29 20:02:50 +0200

    Update library.tsx

--- src/pages/library/library.tsx
+++ src/pages/library/library.tsx
@@ -1,3 +1,19 @@
+import { createMemo, createSignal, For, JSXElement, Show } from 'solid-js'
+import { Outlet, useNavigate, NavLink, useMatch } from 'solid-app-router'
+import { CSSTransition } from '~/components/css-transition/css-transition'
+import { Icon } from '~/components/icon/icon'
+import { IconButton } from '~/components/icon-button/icon-button'
+import { useMenu } from '~/components/menu/menu'
+import { useEntitiesStore, useLibraryStore } from '~/stores/stores'
+import { MessageBanner } from '~/components/message-banner/message-banner'
+import { LibraryPageConfig, CONFIG } from './config'
+import { MusicItemType } from '~/types/types'
+import { useMapRouteToValue } from '~/helpers/router-match'
+import { Scaffold } from '~/components/scaffold/scaffold'
+import { AppTopBar } from '~/components/app-top-bar/app-top-bar'
+import { createMediaQuery } from '~/helpers/hooks/create-media-query'
+import { clx, IS_DEVICE_A_MOBILE } from '~/utils'
+import * as styles from './library.css'
 import React, { useEffect, useState } from "react";
 
 const Library = () => {
