9017c363fd7ee28697b943c3bcf55998fbb18549
cheesesuccess <natashasibanda2025@outlook.com>
2025-07-29 21:06:30 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,11 +16,6 @@ const createMScreenshot = (name: string, sizes: string) => ({
 })
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 5173
-  },
-
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
