a99c349c9c432bcf865df71001a97f00f0c0946e
cheesesuccess <natashasibanda2025@outlook.com>
2025-08-01 17:22:05 +0200

    Update static-songs.js

--- src/data/static-songs.js
+++ src/data/static-songs.js
@@ -4,11 +4,11 @@ export const STATIC_SONGS = [
   {
     title: "Demo Track 1",
     artist: "Unknown Artist",
-    src: "/songs/track1.mp3",
+    src: "/songs/write a song about drinking 43 cans of D.mp3",
   },
   {
     title: "Demo Track 2",
     artist: "Unknown Artist",
-    src: "/songs/track2.mp3",
+    src: "/songs/Sharing is caring.mp3",
   },
 ];
