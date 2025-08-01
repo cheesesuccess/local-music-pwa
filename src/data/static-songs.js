95f955e3459711603c06a11ff891f9828804cba1
cheesesuccess <natashasibanda2025@outlook.com>
2025-08-01 17:05:39 +0200

    Create static-songs.js

---
+++ src/data/static-songs.js
@@ -0,0 +1,14 @@
+// src/data/static-songs.ts
+
+export const STATIC_SONGS = [
+  {
+    title: "Demo Track 1",
+    artist: "Unknown Artist",
+    src: "/songs/track1.mp3",
+  },
+  {
+    title: "Demo Track 2",
+    artist: "Unknown Artist",
+    src: "/songs/track2.mp3",
+  },
+];
